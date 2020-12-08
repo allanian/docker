1943795b523ffc00a47dbeb7d1ce9da5953a7bfa
allanian <shade45@mail.ru>
2020-12-08 17:12:56 +0300

    Create backup.sh

---
+++ docker/gitlab/backup/backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker exec -t gitlab gitlab-backup create;
+#docker exec -t gitlab gitlab-backup create SKIP=registry;
+docker exec -t gitlab gitlab-ctl backup-etc /var/opt/gitlab/backups;
+rsync -azP --no-perms -0 --no-o --no-g /srv/gitlab/data/backups/ /mnt/remote_backups;
+# delete local backups
+find "/srv/gitlab/data/backups/" -maxdepth 1 -type f -mmin +((60*23)) -name '*.tar' -delete;
