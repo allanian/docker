c1f0441dad7f7959499ec8936aab9947c2989f5b
allanian <shade45@mail.ru>
2021-02-04 17:19:09 +0300

    Update backup.sh

--- repo/gitlab/backup/backup.sh
+++ repo/gitlab/backup/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker exec -t gitlab gitlab-backup create;
-#docker exec -t gitlab gitlab-backup create SKIP=registry;
+docker exec -t gitlab gitlab-backup create SKIP=registry,pages,lfs;
 docker exec -t gitlab gitlab-ctl backup-etc /var/opt/gitlab/backups;
-rsync -azP --no-perms -0 --no-o --no-g /srv/gitlab/data/backups/ /mnt/remote_backups;
+rsync -azP --no-perms -0 --no-o --no-g /srv/gitlab/data/backups/ /mnt/remote_backups; 
 # delete local backups
-find "/srv/gitlab/data/backups/" -maxdepth 1 -type f -mmin +$((60*23)) -name '*.tar' -delete;
+find "/srv/gitlab/data/backups/" -maxdepth 1 -type f -mmin +$((120)) -name '*.tar' -delete;
+find "/mnt/remote_backups/" -maxdepth 1 -type f -mmin +$((120*192)) -name '*.tar' -delete;
